a1d634f2c7312c20fa0a66d0368ddcb53a7786c5
Maria <114153517+eyesmaybeyes@users.noreply.github.com>
2023-06-13 22:45:49 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./components/Header/Header.css";
-import "./components/Footer/Footer.css";
-import { Header } from "./components/Header/Header.jsx";
-import { Footer } from "./components/Footer/Footer.jsx";
+import './components/Header/Header.css';
+import './components/Footer/Footer.css';
+import { Header } from './components/Header/Header.jsx';
+import { Footer } from './components/Footer/Footer.jsx';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+        </div>
+    );
 }
 
 export default App;
